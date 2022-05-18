0b54864e21409b1f365d67c91921d7cfe2e86f34
XsIsT97 <galin_3xg@abv.bg>
2022-05-18 21:16:46 +0300

    Add Files

---
+++ Multidimensional Arrays/Multidimensional Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Multidimensional_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]
+            {
+                { 10, 20 , 30},
+                { 40, 50 , 60},
+            };
+
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write(matrix[r, c]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
