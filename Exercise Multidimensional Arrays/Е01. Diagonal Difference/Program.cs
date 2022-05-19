8561db5ad1a04abeec8f874222abee7b2359b5a2
XsIsT97 <galin_3xg@abv.bg>
2022-05-19 21:01:00 +0300

    Add Files

---
+++ Exercise Multidimensional Arrays/Е01. Diagonal Difference/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Е01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            // напълваме матрицата
+            int[,] numbers = new int[n, n];
+
+            FillMatrix(numbers);
+            // сума от главния диагонал ред == колона
+            int sumPrimaryDiagonal = 0;
+            // сума от второстепенния диафонал ред + колона = n - 1
+            int sumSecondaryDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    int number = numbers[row, col];
+
+                    if (row == col)
+                    {
+                        // числото е на главния диагонал
+                        sumPrimaryDiagonal += number;
+                    }
+                    
+                    if (row + col == n - 1)
+                    {
+                        // числото е на второстепенния
+                        sumSecondaryDiagonal += number;
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal));
+        }
+
+        private static void FillMatrix(int[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < rowData.Length; col++)
+                {
+                    numbers[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
---
+++ Exercise Multidimensional Arrays/Е02.  2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Е02.__2X2_Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int rows = int.Parse(input.Split()[0]);
+            int cols = int.Parse(input.Split()[1]);
+
+            string[,] matrix = new string[rows, cols];
+            FillMatrix(matrix);
+
+            int count = 0; // брой на матриците 2 x 2
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] 
+                        && matrix[row, col] == matrix[row + 1, col] 
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        // Матрица 2x2, на която всички елементи са еднакви
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] rowData = Console.ReadLine().Split();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
