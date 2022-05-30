f24beb65080aec7c8af2e2c2c8cf2a11c255ed6e
XsIsT97 <galin_3xg@abv.bg>
2022-05-30 21:19:35 +0300

    Add Files

---
+++ Functional Programming/Е02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Е02._Sum_Numbers
+{
+    internal class Program
+    {
+        static int Parse(string str) => int.Parse(str);
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] nums = input.Split(", ").Select(Parse).ToArray();
+
+            Console.WriteLine(nums.Count());
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
