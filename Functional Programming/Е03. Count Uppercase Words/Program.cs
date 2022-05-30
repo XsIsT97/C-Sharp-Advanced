4a9ac88f9f329454b693c9e6d8c028f8beb4a057
XsIsT97 <galin_3xg@abv.bg>
2022-05-30 21:20:10 +0300

    Add Files

---
+++ Functional Programming/Е03. Count Uppercase Words/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Е03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static Func<string, bool> isFirstLetterCapital =
+        x => x.Length > 0 && char.IsUpper(x[0]);
+
+            static void Main() =>
+                Console.WriteLine(
+                    String.Join(
+                        "\r\n",
+                        Console.ReadLine()
+                        .Split(" ")
+                        .Where(isFirstLetterCapital)
+                    )
+                );
+    }
+}
