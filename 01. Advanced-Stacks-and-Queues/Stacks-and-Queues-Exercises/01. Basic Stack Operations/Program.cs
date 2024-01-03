d6e97d5f1da5f4ff82b4d6a460c87a387c82e50d
Galin <galin_3xg@abv.bg>
2024-01-03 19:55:55 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Exercises/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input =Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int N = input[0];
+            int S = input[1];
+            int X = input[2];
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+
+            for (int i = 0; i < S; i++)
+            {
+                if (stack.Any())
+                {
+                    stack.Pop();
+                }
+            }
+
+            if (stack.Contains(X))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Any() ? stack.Min() : 0);
+            }
+        }
+    }
+}
