62d111c7578754a75f36acc2a2f7021e0cf22535
Galin <galin_3xg@abv.bg>
2024-01-04 18:09:39 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsPassingEveryGreenLight = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            Queue<string> trafficQueue = new Queue<string>();
+            int countOfCarsThatPassed = 0;
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < carsPassingEveryGreenLight; i++)
+                    {
+                        if (trafficQueue.Count > 0)
+                        {
+                            countOfCarsThatPassed++;
+                            string car = trafficQueue.Dequeue();
+                            Console.WriteLine($"{car} passed!");
+                        }
+                    }
+                }
+                else
+                {
+                    trafficQueue.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{countOfCarsThatPassed} cars passed the crossroads.");
+        }
+    }
+}
