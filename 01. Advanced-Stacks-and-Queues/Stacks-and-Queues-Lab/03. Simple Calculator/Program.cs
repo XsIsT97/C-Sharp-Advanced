27681d16655a1e69a8144617865ac1c6ec69d306
Galin <galin_3xg@abv.bg>
2024-01-04 18:07:30 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> calculate = new Stack<string>(Console.ReadLine().Split(' ').Reverse());
+
+            while (calculate.Count > 1)
+            {
+                int firsNumber = int.Parse(calculate.Pop());
+                string command = calculate.Pop();
+                int secondNumber = int.Parse(calculate.Pop());
+
+                if (command == "-")
+                {
+                    var subtract = (firsNumber - secondNumber).ToString();
+                    calculate.Push(subtract);
+                }
+                else if (command == "+")
+                {
+                    var addition = (firsNumber + secondNumber).ToString();
+                    calculate.Push(addition);
+                }
+            }
+            Console.WriteLine(calculate.Peek());
+        }
+    }
+}
