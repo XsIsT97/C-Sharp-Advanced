5a8a83e24bb69857b98dc0da202ac29aad495b4e
Galin <galin_3xg@abv.bg>
2024-01-04 18:06:22 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Lab/01. Reverse a String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<string> chars = new Stack<string>();
+
+            foreach (var c in input)
+            {
+                chars.Push(c.ToString());
+            }
+
+            while (chars.Count > 0)
+            {
+                Console.Write(chars.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
