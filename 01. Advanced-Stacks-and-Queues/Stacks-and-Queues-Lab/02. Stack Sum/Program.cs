85b7139332407c49feeaf0d2fa28011991e283da
Galin <galin_3xg@abv.bg>
2024-01-04 18:07:06 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            string command = Console.ReadLine().ToLower();
+            while (command != "end")
+            {
+                string[] argument = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string action = argument[0];
+
+                if (action == "add")
+                {
+                    numbers.Push(int.Parse(argument[1]));
+                    numbers.Push(int.Parse(argument[2]));
+                }
+                else if (action == "remove")
+                {
+                    int numberToRemove = int.Parse(argument[1]);
+
+                    if (numberToRemove <= numbers.Count)
+                    {
+                        for (int i = 0; i < numberToRemove; i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+            int sum = numbers.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
