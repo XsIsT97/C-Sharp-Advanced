5c458cbf49dc0e41e95a9279b991e8a8dfb7220e
Galin <galin_3xg@abv.bg>
2024-01-04 18:08:20 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
