e68455dcacda2717da8dada79cfda4fa7f4ade26
Galin <galin_3xg@abv.bg>
2024-01-04 18:07:53 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exprecion = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < exprecion.Length; i++)
+            {
+                if (exprecion[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (exprecion[i] == ')')
+                {
+                    int openBracketIndex = stack.Pop();
+                    int closeBracketIndex = i;
+
+                    Console.WriteLine(exprecion.Substring(openBracketIndex, closeBracketIndex - openBracketIndex + 1));
+                }
+            }
+        }
+    }
+}
