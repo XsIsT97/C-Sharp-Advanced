585024d8aafb755987b6d9ae1769bdc56a6a55bf
Galin <galin_3xg@abv.bg>
2024-01-05 17:30:02 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Exercises/04. Fast Food/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(orders.Max());
+
+            Queue<int> ordersQueue = new Queue<int>(orders);
+
+            while (ordersQueue.Count > 0)
+            {
+                int nextOrder = ordersQueue.Peek();
+
+                if (quantityOfFood - nextOrder >= 0)
+                {
+                    quantityOfFood -= nextOrder;
+                    ordersQueue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(ordersQueue.Count == 0
+                ? "Orders complete"
+                : $"Orders left: {string.Join(' ', ordersQueue)}");
+        }
+    }
+}
