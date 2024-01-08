4489b1dbf56dc8396289a8556f27c2facfb86139
Galin <galin_3xg@abv.bg>
2024-01-08 15:37:01 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Exercises/12. Cups and Bottles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapasity = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] bottlesAray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> cups = new Queue<int>(cupsCapasity);
+            Stack<int> bottles = new Stack<int>(bottlesAray);
+
+            int wastedWater = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                int currentCupCapacity = cups.Peek();
+
+                while (currentCupCapacity > 0)
+                {
+                    currentCupCapacity -= bottles.Pop();
+                }
+                cups.Dequeue();
+                wastedWater += Math.Abs(currentCupCapacity);
+            }
+
+            Console.WriteLine(cups.Any() 
+                ? $"Cups: {string.Join(' ', cups)}" 
+                : $"Bottles: {string.Join(' ', bottles)}");
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
