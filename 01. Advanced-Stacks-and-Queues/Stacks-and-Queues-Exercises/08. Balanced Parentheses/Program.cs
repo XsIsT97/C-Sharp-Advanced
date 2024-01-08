05194e1c0a3db364ce4b0353991019468f9886be
Galin <galin_3xg@abv.bg>
2024-01-08 15:35:41 +0200

    Add file

---
+++ 01. Advanced-Stacks-and-Queues/Stacks-and-Queues-Exercises/08. Balanced Parentheses/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+
+            Stack<char> open = new Stack<char>();
+            bool isBalanced = true;
+            foreach (var bracket in sequence)
+            {
+                if (bracket == '(' || bracket == '{' || bracket == '[')
+                {
+                    open.Push(bracket);
+                }
+                else
+                {
+                    if (!open.Any())
+                    {
+                        isBalanced = false;
+                        Console.WriteLine("NO");
+                        break;
+                    }
+                    else if (bracket == ')' && open.Peek() == '(')
+                    {
+                        open.Pop();
+                    }
+                    else if (bracket == '}' && open.Peek() == '{')
+                    {
+                        open.Pop();
+                    }
+                    else if (bracket == ']' && open.Peek() == '[')
+                    {
+                        open.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                        Console.WriteLine("NO");
+                        break;
+                    }
+                }
+            }
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
