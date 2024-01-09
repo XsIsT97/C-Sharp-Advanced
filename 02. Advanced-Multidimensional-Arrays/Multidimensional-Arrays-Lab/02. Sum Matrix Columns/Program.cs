846b8a1d70cdb001ef3a0315baecbfd0d63c0be2
Galin <galin_3xg@abv.bg>
2024-01-09 20:10:46 +0200

    Add file

---
+++ 02. Advanced-Multidimensional-Arrays/Multidimensional-Arrays-Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] matrixParams = Console.ReadLine().Split(", ");
+            int rows = int.Parse(matrixParams[0]);
+            int cols = int.Parse(matrixParams[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] numbers = Console.ReadLine().Split(" ");
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(numbers[col]);
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int colSum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    colSum += matrix[row, col];
+                }
+
+                Console.WriteLine(colSum);
+            }
+        }
+    }
+}
