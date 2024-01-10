6dfd3e5089924bd8a54a742d0883ed28d89ee5b3
Galin <galin_3xg@abv.bg>
2024-01-10 16:57:25 +0200

    Add file

---
+++ 02. Advanced-Multidimensional-Arrays/Multidimensional-Arrays-Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string[] numbers = Console.ReadLine().Split(' ');
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = int.Parse(numbers[col]);
+                }
+            }
+
+            int diagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    diagonalSum += matrix[row, row];
+                    break;
+                }
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}
