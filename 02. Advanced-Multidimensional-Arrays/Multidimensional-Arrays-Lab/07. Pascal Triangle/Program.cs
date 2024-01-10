986216eda834bd18a331f79d26cf7d5543fbfaca
Galin <galin_3xg@abv.bg>
2024-01-10 16:59:57 +0200

    Add file

---
+++ 02. Advanced-Multidimensional-Arrays/Multidimensional-Arrays-Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                pascalTriangle[i] = new long[i + 1];
+                pascalTriangle[i][0] = 1;
+                pascalTriangle[i][pascalTriangle[i].Length - 1] = 1;
+
+                for (int j = 1; j < pascalTriangle[i].Length - 1; j++)
+                {
+                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                }
+            }
+
+            foreach (var row in pascalTriangle)
+            {
+                Console.WriteLine(string.Join(' ', row));
+            }
+        }
+    }
+}
