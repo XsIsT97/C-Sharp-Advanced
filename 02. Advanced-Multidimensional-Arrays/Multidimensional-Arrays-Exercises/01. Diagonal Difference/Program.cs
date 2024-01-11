bf000639bd8dfeb9d9215270a023fabddd36c77f
Galin <galin_3xg@abv.bg>
2024-01-11 18:34:17 +0200

    Add file

---
+++ 02. Advanced-Multidimensional-Arrays/Multidimensional-Arrays-Exercises/01. Diagonal Difference/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                primaryDiagonalSum += rowData[row];
+                secondaryDiagonalSum += rowData[n - row - 1];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+    }
+}
