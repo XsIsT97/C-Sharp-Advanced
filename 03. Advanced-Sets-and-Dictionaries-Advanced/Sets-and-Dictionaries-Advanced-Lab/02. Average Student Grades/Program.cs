cd5739a087cb3fc67955f54f319fe6cc98c7fe9b
Galin <galin_3xg@abv.bg>
2024-01-15 18:55:12 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Average_Student_Grades
+{
+    static void Main(string[] args)
+    {
+        int numberOfStudents = int.Parse(Console.ReadLine());
+        Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            string[] studentData = Console.ReadLine().Split(' ');
+            string studentName = studentData[0];
+            decimal studentGrade = decimal.Parse(studentData[1]);
+
+            if (!students.ContainsKey(studentName))
+            {
+                students[studentName] = new List<decimal>();
+            }
+            students[studentName].Add(studentGrade);
+        }
+
+        foreach (var student in students)
+        {
+            decimal averageGrade = student.Value.Average();
+            string gradesAsString = string.Join(" ", student.Value.Select(grade => grade.ToString("f2")));
+
+            Console.WriteLine($"{student.Key} -> {gradesAsString} (avg: {averageGrade:f2})");
+        }
+    }
+}
