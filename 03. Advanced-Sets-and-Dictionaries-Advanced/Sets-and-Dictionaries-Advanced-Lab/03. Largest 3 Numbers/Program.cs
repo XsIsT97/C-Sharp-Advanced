ae70262dbad8090bc2a3a6274b16cadb168cc1a1
Galin <galin_3xg@abv.bg>
2024-01-16 21:58:35 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .Take(3)
+                .ToArray();
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
