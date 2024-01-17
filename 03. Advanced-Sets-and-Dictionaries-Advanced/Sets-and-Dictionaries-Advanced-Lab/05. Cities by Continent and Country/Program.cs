d73ccde23a93e891fb191f0ee87913ef9dcb41b0
Galin <galin_3xg@abv.bg>
2024-01-17 17:02:04 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> world = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string continent = data[0];
+                string country = data[1];
+                string city = data[2];
+
+                if (!world.ContainsKey(continent))
+                {
+                    world[continent] = new Dictionary<string, List<string>>();
+                }
+
+                if (!world[continent].ContainsKey(country))
+                {
+                    world[continent][country] = new List<string>();
+                }
+                world[continent][country].Add(city);
+            }
+
+            foreach (var continent in world)
+            {
+                Console.WriteLine(continent.Key + ":");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
