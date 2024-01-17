a252931b4d1d5fc969cf8a9521ca5a9c5ee7fa46
Galin <galin_3xg@abv.bg>
2024-01-17 17:02:36 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            HashSet<string> parking = new HashSet<string>();
+
+            while (command != "END")
+            {
+                string[] listOfCars = command.Split(", ");
+                string action = listOfCars[0];
+                string carNumber = listOfCars[1];
+
+                if (action == "IN")
+                {
+                    parking.Add(listOfCars[1]);
+                }
+                else
+                {
+                    parking.Remove(listOfCars[1]);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
