fae6e77ef4d886f193b986309f872d4fe328bd9a
Galin <galin_3xg@abv.bg>
2024-01-18 18:43:49 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Exercises/02. Sets of Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] counts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = counts[0];
+            int m = counts[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var list = firstSet.Intersect(secondSet);
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
