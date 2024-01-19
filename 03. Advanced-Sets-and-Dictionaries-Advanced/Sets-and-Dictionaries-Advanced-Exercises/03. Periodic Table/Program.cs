ad3db7a1b21dd7b0eb08c043d1ff5ff5db9dc027
Galin <galin_3xg@abv.bg>
2024-01-19 20:19:03 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Exercises/03. Periodic Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> element = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elemnts = Console.ReadLine().Split(' ');
+
+                foreach (var item in elemnts)
+                {
+                    element.Add(item);
+                }
+            }
+
+            var orderedElements = element.OrderBy(el => el);
+
+            Console.WriteLine(string.Join(' ', orderedElements));
+        }
+    }
+}
