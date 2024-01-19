559b40da44f75ce51b8940e41d3888fbb04f8d3c
Galin <galin_3xg@abv.bg>
2024-01-19 20:19:36 +0200

    Add file

---
+++ 03. Advanced-Sets-and-Dictionaries-Advanced/Sets-and-Dictionaries-Advanced-Exercises/05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+
+                if (!symbols.ContainsKey(symbol))
+                {
+                    symbols[symbol] = 0;
+                }
+                symbols[symbol]++;
+            }
+
+            foreach (var (symbol, count) in symbols)
+            {
+                Console.WriteLine($"{symbol}: {count} time/s");
+            }
+        }
+    }
+}
