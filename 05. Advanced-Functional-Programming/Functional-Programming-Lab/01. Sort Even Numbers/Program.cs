0730670a3fef2ff6bd9e7040c9cfb2033a5db170
Galin <galin_3xg@abv.bg>
2024-01-21 16:56:30 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var evenNumbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(num => num % 2 == 0)
+                .OrderBy(x => x);
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
