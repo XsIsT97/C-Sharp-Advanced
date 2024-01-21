36bf68dc468aaedd58c9388b55676edee33b18f9
Galin <galin_3xg@abv.bg>
2024-01-21 16:57:16 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Lab/04. Add VAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVatFunc = price => price * 1.2;
+            var prices = Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(addVatFunc)
+                .ToArray();
+
+            foreach (var item in prices)
+            {
+                Console.WriteLine($"{item:F2}");
+            }
+        }
+    }
+}
