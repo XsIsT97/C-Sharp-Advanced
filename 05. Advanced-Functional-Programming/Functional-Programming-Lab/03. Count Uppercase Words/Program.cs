9dd41ba85abe34ee31bb39164473459582076a77
Galin <galin_3xg@abv.bg>
2024-01-21 16:57:00 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => char.IsUpper(word[0]))
+                .ToArray();
+
+            foreach (string item in text)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
