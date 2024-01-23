be4edc239379bf2c9781b5d99bc696c887069d7a
Galin <galin_3xg@abv.bg>
2024-01-23 19:45:48 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Exercises/06. Reverse and Exclude/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int divider = int.Parse(Console.ReadLine());
+            Predicate<int> filter = num => num % divider != 0;
+
+            numbers.Reverse();
+            numbers = MyWhere(numbers, filter);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        private static List<int> MyWhere(List<int> numbers, Predicate<int> filter)
+        {
+            List<int> newList = new List<int>();
+
+            foreach (var item in numbers)
+            {
+                if (filter(item))
+                {
+                    newList.Add(item);
+                }
+            }
+
+            return newList;
+        }
+    }
+}
