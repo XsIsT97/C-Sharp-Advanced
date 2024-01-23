4292b48f7dbc7d9845fc52406ba2fcf32899cb80
Galin <galin_3xg@abv.bg>
2024-01-23 19:44:14 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Exercises/02. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printName = Console.WriteLine;
+
+            string[] names = Console.ReadLine().Split(' ');
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
