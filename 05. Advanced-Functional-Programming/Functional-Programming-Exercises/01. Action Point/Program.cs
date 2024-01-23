21ea253d2d7317d04ae9dd27432f8275afd244ec
Galin <galin_3xg@abv.bg>
2024-01-23 19:43:49 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Exercises/01. Action Point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printName = Console.WriteLine;
+
+            string[] name = Console.ReadLine().Split(' ');
+
+            foreach (var item in name)
+            {
+                printName(item);
+            }
+        }
+    }
+}
