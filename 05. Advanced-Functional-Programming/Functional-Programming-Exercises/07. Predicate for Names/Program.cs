a6bd27a088fa1bd1c10c53a8407bbb4da5c8c04b
Galin <galin_3xg@abv.bg>
2024-01-23 19:46:11 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Exercises/07. Predicate for Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetLenght = int.Parse(Console.ReadLine());
+            List<string> name = Console.ReadLine().Split(' ').ToList();
+
+            Func<string, bool> function = name => name.Length <= targetLenght;
+
+            name = name.Where(function).ToList();
+
+            Action<List<string>> print = name => Console.WriteLine(string.Join(Environment.NewLine, name));
+
+            print(name);
+        }
+    }
+}
