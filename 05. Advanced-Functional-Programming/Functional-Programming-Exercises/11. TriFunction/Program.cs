f2c2f7234cea27707a6902cd564c1bcefad59bd8
Galin <galin_3xg@abv.bg>
2024-01-24 15:15:04 +0200

    Add file

---
+++ 05. Advanced-Functional-Programming/Functional-Programming-Exercises/11. TriFunction/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetNumber = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Func<string, int, bool> func = (name, target) =>
+            {
+                int sum = 0;
+                foreach (var symbol in name)
+                {
+                    sum += symbol;
+                    if (sum >= target)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            string name = GetName(func, names, targetNumber);
+            Console.WriteLine(name);
+        }
+
+        private static string GetName(Func<string, int, bool> func, List<string> names, int targetNumber)
+        {
+            foreach (var name in names)
+            {
+                if (func(name, targetNumber))
+                {
+                    return name;
+                }
+            }
+            return null;
+        }
+    }
+}
