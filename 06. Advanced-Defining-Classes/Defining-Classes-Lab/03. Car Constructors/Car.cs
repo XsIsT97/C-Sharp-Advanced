481d579b3e924592ab8a597faae34792cbd1d340
Galin <galin_3xg@abv.bg>
2024-01-26 15:29:01 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+
+        public void Drive(double distance)
+        {
+            double requiredFuel = distance * FuelConsumption;
+            if (fuelQuantity - requiredFuel >= 0)
+            {
+                FuelQuantity -= requiredFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Lab/03. Car Constructors/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var make = Console.ReadLine();
+            var model = Console.ReadLine();
+            var year = int.Parse(Console.ReadLine());
+            var fuelQuantity = double.Parse(Console.ReadLine());
+            var fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "X5";
+            car.Year = 2006;
+            car.FuelQuantity = 80;
+            car.FuelConsumption = fuelConsumption;
+            Console.WriteLine(car.WhoAmI());
+            Car firstCar = new Car();
+
+            Console.WriteLine(firstCar.WhoAmI());
+
+            Car secondCar = new Car(make, "Yaris", year);
+            Console.WriteLine(secondCar.WhoAmI());
+
+            Car thirdCar = new Car("Opel", model, 2016, 300, fuelConsumption);
+
+            thirdCar.Drive(200);
+            Console.WriteLine(thirdCar.WhoAmI());
+        }
+    }
+}
