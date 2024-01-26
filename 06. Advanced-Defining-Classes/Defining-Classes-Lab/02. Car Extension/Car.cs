11ccf0257fd47d6ff9d00f745ebfdb45b9374dd2
Galin <galin_3xg@abv.bg>
2024-01-26 15:28:48 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Lab/02. Car Extension/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double requiredFuel = distance * FuelConsumption;
+            if (fuelQuantity - requiredFuel >= 0)
+            {
+                FuelQuantity -= requiredFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 10;
+
+            car.Drive(20);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
