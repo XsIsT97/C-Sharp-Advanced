369a7a43e6a6fad54b31067e3ddd4e0a49c523db
Galin <galin_3xg@abv.bg>
2024-01-27 17:58:33 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person persone1 = new Person()
+            {
+                Name = "Peter",
+                Age = 20
+            };
+
+            Console.WriteLine(persone1.Name+ " " +persone1.Age);
+        }
+    }
+}
