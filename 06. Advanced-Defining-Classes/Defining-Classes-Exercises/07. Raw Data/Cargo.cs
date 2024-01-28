1a60eea234252e57bf765485140449877f091faf
Galin <galin_3xg@abv.bg>
2024-01-28 20:14:07 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/07. Raw Data/Cargo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Raw_Data
+{
+    internal class Cargo
+    {
+        public Cargo(int weight, string type)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        //Cargo: a class with two properties – type and weight 
+        public string Type { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/07. Raw Data/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Raw_Data
+{
+    internal class Tire
+    {
+        public Tire(double pressure, int age)
+        {
+            Age = age;
+            Pressure = pressure;
+        }
+
+        public int Age { get; set; }
+        public double Pressure { get; set; }
+    }
+}
