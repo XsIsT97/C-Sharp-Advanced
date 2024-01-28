3b157b881e8a77a12a960ba8d7aff9d19ee44f11
Galin <galin_3xg@abv.bg>
2024-01-28 23:30:28 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/10. SoftUni Parking/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._SoftUni_Parking
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine($"Make: {this.Make}");
+            output.AppendLine($"Model: {this.Model}");
+            output.AppendLine($"HorsePower: {this.HorsePower}");
+            output.Append($"RegistrationNumber: {this.RegistrationNumber}");
+            return output.ToString();
+        }
+    }
+}
