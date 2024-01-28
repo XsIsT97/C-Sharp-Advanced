bb3220ee3949212efe5c330e747f39679bd5979d
Galin <galin_3xg@abv.bg>
2024-01-28 20:09:14 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Oldest_Family_Member
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
