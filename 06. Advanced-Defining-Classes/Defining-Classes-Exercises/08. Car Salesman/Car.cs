331b9d725448a9559bf6325d48d2d5f49eb7c4a5
Galin <galin_3xg@abv.bg>
2024-01-28 20:14:24 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/08. Car Salesman/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._Car_Salesman
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        public Car(string model, Engine engine)
+        {
+            Model = model;
+            Engine = engine;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Model}:");
+            sb.AppendLine($"  {Engine.Model}:");
+            sb.AppendLine($"    Power: {Engine.Power}");
+            sb.AppendLine(Engine.Displacement == 0 ? "    Displacement: n/a" : $"    Displacement: {Engine.Displacement}");
+            sb.AppendLine(Engine.Efficiency == null ? "    Efficiency: n/a" : $"    Efficiency: {Engine.Efficiency}");
+            sb.AppendLine(Weight == 0 ? "  Weight: n/a" : $"  Weight: {Weight}");
+            sb.Append(Color == null ? "  Color: n/a" : $"  Color: {Color}");
+            return sb.ToString();
+        }
+    }
+}
