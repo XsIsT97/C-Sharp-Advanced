7fb758d3bdcb4f06bd2aa27a8a5ecde421f65080
Galin <galin_3xg@abv.bg>
2024-01-28 20:09:58 +0200

    Add file

---
+++ 06. Advanced-Defining-Classes/Defining-Classes-Exercises/05. Date Modifier/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._Date_Modifier
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
