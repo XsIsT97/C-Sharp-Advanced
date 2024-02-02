5d32b8453ed54e09f199524eddeed5882e97213f
Galin <galin_3xg@abv.bg>
2024-02-02 16:52:10 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Exercises/01. Generic Box of String/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Generic_Box_of_String
+{
+    internal class Box<T>
+    {
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ 08. Advanced-Generics/Generics-Exercises/01. Generic Box of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Generic_Box_of_String
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                Box<string> box = new Box<string>();
+                box.Value = s;
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
