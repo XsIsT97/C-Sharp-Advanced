690c6c6de705652993d659959cbe3fb9b8a38760
Galin <galin_3xg@abv.bg>
2024-02-02 15:44:41 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Lab/02. Generic Array Creator/ArrayCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Generic_Array_Creator
+{
+    internal class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = item;
+            }
+            return array;
+        }
+    }
+}
---
+++ 08. Advanced-Generics/Generics-Lab/02. Generic Array Creator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Generic_Array_Creator
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create<string>(5, "Pesho");
+            int[] integers = ArrayCreator.Create<int>(10, 33);
+    }
+}
