7c5a4890b1695fc106d70715ec71b04f6e943e52
Galin <galin_3xg@abv.bg>
2024-02-02 16:52:49 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Exercises/03. Generic Swap Method Strings/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Generic_Swap_Method_Strings
+{
+    internal class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
+
