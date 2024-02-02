66ebe61b6da427d6b5d409ecccb62b9c23f43610
Galin <galin_3xg@abv.bg>
2024-02-02 17:17:58 +0200

    Add file

---
+++ 09. Advanced-Iterators-and-Comparators/Iterators-and-Comparators-Exercises/06. Equality Logic/Persone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._Equality_Logic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+
+            return this.Name == person.Name && this.Age == person.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+    }
+}
