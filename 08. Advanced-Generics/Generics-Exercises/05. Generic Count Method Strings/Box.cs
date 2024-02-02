0680cbcb2e9fc57bf3f389e65152c5d8bb7c2ab3
Galin <galin_3xg@abv.bg>
2024-02-02 16:53:19 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Exercises/05. Generic Count Method Strings/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._Generic_Count_Method_Strings
+{
+    internal class Box<T> where T : IComparable<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+}
