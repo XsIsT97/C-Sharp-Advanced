7425d88dced8075eaef16206e1a50047161874a1
Galin <galin_3xg@abv.bg>
2024-02-02 16:52:31 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Exercises/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Generic_Box_of_Integer
+{
+    internal class Box<T>
+    {
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
---
+++ 08. Advanced-Generics/Generics-Exercises/02. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Generic_Box_of_Integer
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>();
+                box.Value = num;
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
