a47cd58d8f6359e9970781404076ae9e9b1959dc
Galin <galin_3xg@abv.bg>
2024-02-02 15:45:00 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Lab/03. Generic Scale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Generic_Scale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
