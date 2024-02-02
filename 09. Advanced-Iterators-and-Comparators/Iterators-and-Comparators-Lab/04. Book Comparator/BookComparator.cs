c41bd4a4eeffd0b706cf4e42115a2f5affb31e6d
Galin <galin_3xg@abv.bg>
2024-02-02 17:06:25 +0200

    Add file

---
+++ 09. Advanced-Iterators-and-Comparators/Iterators-and-Comparators-Lab/04. Book Comparator/BookComparator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Book_Comparator
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book book1, Book book2)
+        {
+            int result = book1.Title.CompareTo(book2.Title);
+            if (result == 0)
+            {
+                if (book1.Year > book2.Year)
+                {
+                    result = -1;
+                }
+                else if (book1.Year < book2.Year)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
