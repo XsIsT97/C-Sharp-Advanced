ca2ca3c21b29c6eb6883c013c47b78694ce301f5
Galin <galin_3xg@abv.bg>
2024-02-02 15:44:13 +0200

    Add file

---
+++ 08. Advanced-Generics/Generics-Lab/01. Box of T/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    internal class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public int Count => internalList.Count;
+
+        public void Add(T element)
+        {
+            this.internalList.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+            return element;
+        }
+    }
+}
---
+++ 08. Advanced-Generics/Generics-Lab/01. Box of T/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Count);
+        }
+    }
+}
